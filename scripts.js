e0341451912d6239f9bcccb606f62d597de5c8ce
Thaisa-R <thaisascopparo@gmail.com>
2024-01-23 20:23:13 -0300

    projeto jokenpo

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const humanPlayer = (humanChoice) => {
+    plays(humanChoice, playerMachine())
+}
+
+const playerMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]   
+}
+
+const plays = (human, machine) => {
+    console.log(' Humano: ' + human + " Maquina: " + machine)
+
+if(human === machine) {
+    result.innerHTML = "Deu empate!! ​😬​"
+}
+
+else if(
+(human === 'paper' && machine === 'rock') || 
+(human === 'scissors' && machine === 'paper') || 
+(human === 'rock' && machine === 'scissors')) {
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = "Você ganhou!!​ 🎉👏🏼​"
+    
+} else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = "Você perdeu para a Alexa!! 😭​"
+}
+}
